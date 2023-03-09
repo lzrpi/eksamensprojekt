3337b09c1195e8a87a3cfa8a01788a08347f937f
Philip <philipvesterby@gmail.com>
2023-03-09 13:18:23 +0100

    Tilføjede menu del til projektet

---
+++ menu.py
@@ -0,0 +1,7 @@
+#Dette er menu delen til at finde rundt i vores beregnings program 
+print("Hej og velkommen til fysikopgave løseren")
+print("Programmet er udviklet af Mads og Philip fra X21 som en del af eksamensprojekt i programmering C")
+print("Programmet kan løse opgaver indenfor de 3 nedenstående emner")
+print("1. Elektriske Kredsløb")
+print("2. Termodynamik")
+print("3. Bevægelse")
